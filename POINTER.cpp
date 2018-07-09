c3fe10b9ddd4092daf794d321a6050320a23fe59
rafikafika <39395288+rafikafika@users.noreply.github.com>
2018-07-09 16:07:55 +0700

    Add files via upload

---
+++ POINTER.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main (){
+	int x,y; int *px;
+	x = 89;
+	y = x;
+	px = &x;//dan adalah operator alamat
+	cout<<"Nilai x = "<<x<<endl;
+	cout<<"Nilai y = "<<y<<endl;
+	cout<<"Alamat px = "<<px<<endl;
+	cout<<"Nilai px = "<< *px<<endl;
+	getch();
+}
+
